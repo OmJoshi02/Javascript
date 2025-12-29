4c4093f488d3d62e088afa1a28e0c78886b46b7b
OmJoshi02 <omj020705@gmail.com>
2025-12-29 11:32:47 +0530

    Types of datatypes

---
+++ self/Chapter 2/practice_2.1/datatype.js
@@ -0,0 +1,13 @@
+//Demonstrate type of datatypes of the given values
+
+let str1 = "Laurence"; //string
+let str2 = "xvyz"; //string
+let val1 = undefined; //undefined
+let val2 = null; //object
+let num = 219; //number
+
+console.log(typeof(str1));
+console.log(typeof(str2));
+console.log(typeof(val1));
+console.log(typeof(val2));
+console.log(typeof(num));
