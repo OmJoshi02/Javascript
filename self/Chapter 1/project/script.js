d560b4a011f75f50f4ed32c65246ecd749e391e7
OmJoshi02 <omj020705@gmail.com>
2025-12-29 11:27:47 +0530

    js file

---
+++ self/Chapter 1/project/script.js
@@ -0,0 +1,8 @@
+prompt("This is Chapter project")
+prompt("Enter Two numbers : ");
+
+let a = prompt("First number : ");
+let b = prompt("Second number : ");
+
+let c = a+b;
+alert("Sum of both number is : "+c);
