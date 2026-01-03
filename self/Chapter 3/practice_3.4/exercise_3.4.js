0707a0ef0487b7ae7f5d0e300005d5042e38e03b
OmJoshi02 <omj020705@gmail.com>
2026-01-03 19:14:04 +0530

    Object Demonstration

---
+++ self/Chapter 3/practice_3.4/exercise_3.4.js
@@ -0,0 +1,17 @@
+let mycar = {
+    carName : "Lambo",
+    wheels : 4,
+    seats : 2,
+    faster : true,
+    forSale : "California"
+}
+
+let color = "Red"
+
+mycar["color"] = color;
+console.log(mycar);
+
+mycar.forSale = "Colambia";
+console.log(mycar);
+
+
