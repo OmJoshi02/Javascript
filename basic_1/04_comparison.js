ad9e864cb9cbbac6088bc2b42f0541daef7433ba
OmJoshi02 <omj020705@gmail.com>
2026-01-04 23:17:25 +0530

    Comparison

---
+++ basic_1/04_comparison.js
@@ -0,0 +1,30 @@
+console.log(2 > 1); //true
+console.log("2" > 1); //true
+console.log("02" > 1); //true
+
+
+//Special type of conversion comparisons
+//we shouldnot use this
+console.log(null > 0); //false
+console.log(null < 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true  here null is converted to 0
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// === this strictly checks everything does it equal and does it has same datatype
+
+console.log("2" === 2);
+console.log(    2 === 2);
+
+
+
+
+
+
+
+
+
