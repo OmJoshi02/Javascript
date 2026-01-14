88fe517bfefaed37a551c6885197aae26e123672
OmJoshi02 <omj020705@gmail.com>
2026-01-14 19:12:28 +0530

    exercise_3.5

---
+++ self/Chapter 3/practice_3.5/exercise_3.5.js
@@ -0,0 +1,33 @@
+let people = {
+    friends : [],
+}
+
+let fname = "Om";
+let lname = "Joshi"
+let id = 53
+
+people.friends.push({
+    fname : "Om",
+    lname : "Joshi",
+    id : 10
+})
+
+people.friends.push({
+    fname : "Ishita",
+    lname : "Paradeshi",
+    id : 20
+})
+
+people.friends.push({
+    fname : "Snehal",
+    lname : "Kumbhar",
+    id : 30
+})
+
+people.friends.push({
+    fname : "Sanika",
+    lname : "Kumbhar",
+    id : 40
+})
+
+console.log(people)
