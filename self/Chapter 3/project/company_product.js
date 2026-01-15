14725072927ee1f3c2abf58478f10f6ad070aea0
OmJoshi02 <omj020705@gmail.com>
2026-01-15 12:03:48 +0530

    Company product log

---
+++ self/Chapter 3/project/company_product.js
@@ -0,0 +1,33 @@
+let inventory = []
+
+let item1 = {
+    name : "milk",
+    model : "Warna",
+    cost : 34,
+    quantity : 100
+}
+
+let item2 = {
+    name : "chips",
+    model : "Balaji",
+    cost : 5,
+    quantity : 100
+
+}
+
+let item3 = {
+    name : "bread",
+    model : "Sakas",
+    cost : 20,
+    quantity : 100
+
+}
+
+inventory.push(item1)
+inventory.push(item2)
+inventory.push(item3)
+
+// console.log(inventory);
+
+console.log(inventory[2].quantity);
+
